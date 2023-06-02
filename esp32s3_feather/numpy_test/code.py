cddd0633bbf39a550a4ac8426e05516dfac166f6
Karol Duzinkiewicz <karol.duzinkiewicz@dac.digital>
2023-06-02 09:09:34 +0200

    fixed numpy example

--- esp32s3_feather/numpy_test/code.py
+++ esp32s3_feather/numpy_test/code.py
@@ -1,9 +1,14 @@
 import ulab.numpy as np
 
+print('Numpy example')
+
 # matrix inversion example
 a = np.array([[1., 2.], [3., 4.]])
+print(f'a:\n{a}')
 a_inv = np.linalg.inv(a)
+print(f'a_inv:\n{a_inv}')
 
 # sanity check
-assert np.allclose(np.dot(a, a_inv), np.eye(2))
-assert np.allclose(np.dot(a_inv, a), np.eye(2))
+print(f'a * a_inv:\n{np.dot(a, a_inv)}')
+
+print('Done')
