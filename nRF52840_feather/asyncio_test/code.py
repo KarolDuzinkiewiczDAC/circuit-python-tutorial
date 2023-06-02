a95e86fad65687fe3c5bf3e38cc2d7dd8864afb0
Karol Duzinkiewicz <karol.duzinkiewicz@dac.digital>
2023-06-02 16:10:37 +0200

    minor refactor

--- nRF52840_feather/asyncio_test/code.py
+++ nRF52840_feather/asyncio_test/code.py
@@ -24,8 +24,10 @@ async def main():
     # NOTE: It is now the responsibility of the scheduler to start both tasks.
     print_task_1 = asyncio.create_task(print_message(1, 0.25, 10, total_print_count))
     print_task_2 = asyncio.create_task(print_message(2, 0.5, 10, total_print_count))
+
     # wait for all the listed tasks to finish
     await asyncio.gather(print_task_1, print_task_2)
+
     print("done")
 
 asyncio.run(main())
